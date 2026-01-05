42b5206aec80ae60e5540a8b7a66b3c650946558
Muhammad Salman <connect.msalman@gmail.com>
2026-01-05 16:55:57 +0500

    update routes & Counter component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,8 @@
 import { Routes } from '@angular/router';
 import { Welcome } from './welcome/welcome';
+import { Counter } from './counter/counter';
 
 export const routes: Routes = [
-    {
-        path : '' , component : Welcome
-    }
+    { path : '' , component : Welcome },
+    { path : 'counter' , component : Counter}
 ];
--- src/app/counter/counter.ts
+++ src/app/counter/counter.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-counter',
@@ -7,5 +7,19 @@ import { Component } from '@angular/core';
   styleUrl: './counter.css',
 })
 export class Counter {
-
+  count = signal(0);
+  //function for increment
+  increment(){
+    this.count.update(val => val + 1);
+  }
+  //function for decrement
+  decrement(){
+    if(this.count() > 0){
+      this.count.update(val => val - 1);
+    }
+  }
+  //Reset the counter
+  reset(){
+    this.count.set(0);
+  }
 }
